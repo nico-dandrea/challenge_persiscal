7e23326db527f32f906126d9e1ae609fbc1bc631
nico-dandrea <ndev@DESKTOP-KO1N8AQ>
2024-06-27 20:54:10 -0300

    mail facade created and working with log driver

---
+++ resources/views/emails/booking-created.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Mail</title>
+</head>
+<body>
+    <h1>This is a test mail</h1>
+    <p>If you are seeing this, the Mail facade is working!</p>
+</body>
+</html>
